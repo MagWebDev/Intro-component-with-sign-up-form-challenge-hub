a31704e9eb48bc6394e03a5071c2ba4ff45c0ae6
MagWeb <84671218+MagWebDev@users.noreply.github.com>
2021-09-19 22:00:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('input');
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+   inputs.forEach(input =>{
+    if(!input.value) {
+        input.parentElement.classList.add('error')
+    } else {
+        input.parentElement.classList.remove('error')
+    }
+    if(input.type == 'email'){
+           if(validateEmail(input.value)){
+            input.parentElement.classList.remove('error')
+           } else {
+            input.parentElement.classList.add('error')
+           }
+    }
+   }
+  
+    )
+    
+})
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
